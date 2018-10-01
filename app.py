9de2c9f0336affe80503fb93db1d74a6160756d6
adith <adithbharadwaj@gmail.com>
2018-10-01 17:55:53 +0530

    added insert core

---
+++ app.py
@@ -0,0 +1,101 @@
+from flask import Flask, redirect, url_for, request, render_template
+from queries_test import *
+
+app = Flask(__name__)
+
+
+who_is_it = None
+
+
+# login page comes first
+# initial page. the page that is open when we start the server in localhost:5000
+@app.route('/')
+def login():
+	return render_template('login.html')
+
+
+# getting the details of a student from a form (in the html)
+# and using those details to render a template that dynamically displays the details in a page
+# the url in the login.html is of the form <form action = "http://localhost:5000/details" method = "POST">
+# this url specifies where to go. for now, it points to the details page and hence, when the user clicks on submit, the details page is rendered. 
+# if this url is of the form "http://localhost:5000/login", then it goes to the login page described by the login function that is given below. 
+@app.route('/verify', methods = ['POST', 'GET'])
+def details():
+
+	if request.method == 'POST':
+		user = request.form['usn']
+		who = check_login(user)
+
+		if(who == 'User'):
+			who_is_it = 'U'
+		elif(who == 'Core'):
+			who_is_it = 'C'
+		elif(who == 'Office'):
+			who_is_it = 'O'
+
+		else:
+			who_is_it = 'N'
+			return render_template('verify.html', user = 'Not a member. please register. ')
+
+		return render_template('home.html', user = who)
+
+
+@app.route('/insertmembers')
+def insert_members():
+	return render_template('insert_members.html')
+
+
+@app.route('/insertcore')
+def insert_core_members():
+	return render_template('insert_core.html')
+
+
+@app.route('/InsertMembersForm', methods = ['POST', 'GET'])
+def insert_members_form():
+
+	if(request.method == 'POST'):
+		name = request.form['name']
+		usn = request.form['usn']
+		year = request.form['year']
+		branch = request.form['branch']
+		sem = request.form['semester']
+		no_events = request.form['number_of_events_attended']
+		no_of_comps = request.form['number_of_competitions']
+
+		insert_student(usn, name, year, sem, branch, no_events, no_of_comps)
+
+		return render_template('home.html')
+
+@app.route('/InsertCoreForm', methods = ['POST', 'GET'])
+def insert_core_form():
+
+	if(request.method == 'POST'):
+		name = request.form['name']
+		usn = request.form['usn']
+		year = request.form['year']
+		branch = request.form['branch']
+		sem = request.form['semester']
+		pod = request.form['pod']
+
+		insert_core(usn, name, year, sem, branch, pod)
+
+		return render_template('home.html')
+
+
+
+
+# redirects the user who logs in to the page that contains the hello() function (the /user/<name> page)
+@app.route('/login', methods = ['POST', 'GET'])
+def log():
+
+	if request.method == 'POST':
+		user = request.form['Name']
+		return redirect(url_for('hello', name = user))
+	else:
+		val = request.args.get('name')
+		return redirect(url_for('student_details'))
+
+
+if __name__ == '__main__':
+	app.debug = True # debug is used for developing since it allows us to make changes in the python file without having to restart the server again and again.
+	app.run()
---
+++ queries_test.py
@@ -0,0 +1,129 @@
+import sqlite3
+
+def create_db():
+
+
+	#creating tables in the database
+	cur.execute("CREATE TABLE students(usn varchar(11) primary key, name varchar(20), year int, sem int, branch varchar(20), no_of_events int, no_of_wins int)")
+	cur.execute("insert into students values(?, ?, ?, ?, ?, ?, ?)", ["4ni16cs003", 'adith', 2016, 5, 'CS', 1, 0])
+
+	cur.execute("CREATE TABLE core(usn varchar(11) primary key, name varchar(20), year int, sem int, branch varchar(20), pod varchar(20))")
+	cur.execute("insert into core values(?, ?, ?, ?, ?, ?)", ["4ni16cs025", 'bindu', 2016, 5, 'CS', 'Marketing'])
+
+	cur.execute("CREATE TABLE office(usn varchar(11) primary key, name varchar(20), year int, sem int, branch varchar(20), designation varchar(20))")
+	cur.execute("insert into office values(?, ?, ?, ?, ?, ?)", ["4ni16cs010", 'akshat', 2016, 5, 'CS', 'Secretary'])
+
+	cur.execute("CREATE TABLE events(eventID int, name varchar(20), event_date date, venue varchar(20), concept varchar(20))")
+	cur.execute("insert into events values(?, ?, ?, ?, ?)", [1, 'Rags to Riches', '2018-09-08', 'M.V hall', 'Idea Generation'])
+
+	conn.commit()
+
+
+def connect():
+
+	conn = sqlite3.connect("./onyx.db")
+	cur = conn.cursor()
+
+	'''
+	cur.execute("select * from students")
+	data = cur.fetchall()
+
+	for r in data:
+		print(r)
+
+	cur.execute("select * from core")
+	data = cur.fetchall()
+
+	for r in data:
+		print(r)
+
+	cur.execute("select * from office")
+	data = cur.fetchall()
+
+	for r in data:
+		print(r)
+	
+	cur.execute("select * from events")
+	data = cur.fetchall()
+
+	for r in data:
+		print(r)
+	'''
+
+	return conn, cur
+
+
+def insert_student(usn = 'null', name = 'null', year = 'null', sem='null' , branch = 'null', no_events= 'null', no_wins = 'null'):
+
+	conn, cur = connect()
+	cur.execute('insert into students values(?, ?, ?, ?, ?, ?, ?)', [usn, name, year, sem, branch, no_events, no_wins])
+	conn.commit()
+	conn.close()
+
+	select_all_students()
+
+def insert_core(usn = 'null', name = 'null', year = 'null', sem='null' , branch = 'null', pod= 'null'):
+
+	conn, cur = connect()
+	cur.execute('insert into core values(?, ?, ?, ?, ?, ?)', [usn, name, year, sem, branch, pod])
+	conn.commit()
+	conn.close()
+
+	select_all_core()
+
+def select_all_students():
+
+	conn, cur = connect()
+	cur.execute('select * from students')
+
+	data = cur.fetchall()
+	conn.close()
+
+	for r in data:
+		print(r)
+
+	return data
+
+def select_all_core():
+
+	conn, cur = connect()
+	cur.execute('select * from core')
+
+	data = cur.fetchall()
+	conn.close()
+
+	for r in data:
+		print(r)
+
+	return data	
+
+def check_login(usn):
+
+	conn, cur = connect()
+
+	cur.execute("select * from students where usn=?", (str(usn), ))
+	data = cur.fetchall()
+
+	if(data):
+		return 'User'
+
+	else:
+		cur.execute("select * from core where usn=?", (str(usn), ))
+		data1 = cur.fetchall()
+
+		if(data1):
+			return 'Core'
+		else:
+			cur.execute("select * from office where usn=?", (str(usn), ))
+			data2 = cur.fetchall()
+
+			if(data2):
+				return 'Office'
+			else:
+				return 'Not'
+
+	conn.close()
+
+
+
+
